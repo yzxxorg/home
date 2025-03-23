9b96e7698d6c4f2e22c8dc4825b65e2260612fba
yzxxorg <yzxx@aliyun.com>
2025-03-23 11:36:17 +0800

    Update international.v1.hitokoto.cn

--- src/api/index.js
+++ src/api/index.js
@@ -45,7 +45,7 @@ export const getPlayerList = async (server, type, id) => {
 
 // 获取一言数据
 export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
+  const res = await fetch("https://international.v1.hitokoto.cn");
   return await res.json();
 };
 
